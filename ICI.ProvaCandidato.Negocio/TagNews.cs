d81323da332a2f462dcce58a4a2838fec329d703
Cleber Abreu <cleberfasilva@gmail.com>
2024-06-12 20:42:02 -0300

    Add tag news model

---
+++ ICI.ProvaCandidato.Negocio/TagNews.cs
@@ -0,0 +1,9 @@
+﻿namespace ICI.ProvaCandidato.Negocio
+{
+    public class TagNews
+    {
+        public int Id { get; set; }
+        public virtual News News { get; set; }
+        public virtual Tag Tag { get; set; }
+    }
+}
