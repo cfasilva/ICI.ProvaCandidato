64c48dfc29a9a3ca516afc0798d3c72a39159644
Cleber Abreu <cleberfasilva@gmail.com>
2024-06-12 17:51:36 -0300

    feat: add user model

--- ICI.ProvaCandidato.Dados/Migrations/20240612203909_Initial.cs
+++ ICI.ProvaCandidato.Dados/Migrations/20240612203909_Initial.cs
@@ -18,12 +18,30 @@ namespace ICI.ProvaCandidato.Dados.Migrations
                 {
                     table.PrimaryKey("PK_Tags", x => x.Id);
                 });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
                 name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Users");
         }
     }
 }
