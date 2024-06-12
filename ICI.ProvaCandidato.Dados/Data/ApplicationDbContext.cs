0c25e3d525ed2a2eb89589798cad48c0f09935d6
Cleber Abreu <cleberfasilva@gmail.com>
2024-06-12 13:55:13 -0300

    feat: add db context

---
+++ ICI.ProvaCandidato.Dados/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ICI.ProvaCandidato.Dados.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options) { }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
