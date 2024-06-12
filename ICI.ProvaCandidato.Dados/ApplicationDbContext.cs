24bb01751cebc4834b5187f459a045ffdb5dbf0c
Cleber Abreu <cleberfasilva@gmail.com>
2024-06-12 17:50:58 -0300

    refactor: move db context

--- ICI.ProvaCandidato.Dados/ApplicationDbContext.cs
+++ ICI.ProvaCandidato.Dados/ApplicationDbContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ICI.ProvaCandidato.Negocio;
+using Microsoft.EntityFrameworkCore;
 
-namespace ICI.ProvaCandidato.Dados.Data
+namespace ICI.ProvaCandidato.Dados
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
-            base(options) { }
+            base(options)
+        { }
 
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
