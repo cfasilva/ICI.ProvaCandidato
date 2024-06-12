dca2ef02e6161d8abfd13cf1ea6059fc52d7646c
Cleber Abreu <cleberfasilva@gmail.com>
2024-06-12 13:54:06 -0300

    feat: set use urls

--- ICI.ProvaCandidato.Web/Program.cs
+++ ICI.ProvaCandidato.Web/Program.cs
@@ -14,6 +14,7 @@ namespace ICI.ProvaCandidato.Web
 				Host.CreateDefaultBuilder(args)
 						.ConfigureWebHostDefaults(webBuilder =>
 						{
+							webBuilder.UseUrls("http://*:80");
 							webBuilder.UseStartup<Startup>();
 						});
 	}
