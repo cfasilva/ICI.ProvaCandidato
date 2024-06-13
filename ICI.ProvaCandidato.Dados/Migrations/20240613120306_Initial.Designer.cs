6bd24d4f5864519f63b0503578587180cd4bee01
Cleber Abreu <cleberfasilva@gmail.com>
2024-06-13 11:16:54 -0300

    refactor: fix migration

--- ICI.ProvaCandidato.Dados/Migrations/20240613120306_Initial.Designer.cs
+++ ICI.ProvaCandidato.Dados/Migrations/20240613120306_Initial.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ICI.ProvaCandidato.Dados.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240613034031_Initial")]
+    [Migration("20240613120306_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
